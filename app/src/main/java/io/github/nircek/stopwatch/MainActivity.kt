6a2fa57a428ffba2fcd9850ab52da69dc062e39c
Nircek <nircek-2103@protonmail.com>
2019-03-14 17:02:10 +0100

    Separate some code to start() function Add null safety with Android Views

--- app/src/main/java/io/github/nircek/stopwatch/MainActivity.kt
+++ app/src/main/java/io/github/nircek/stopwatch/MainActivity.kt
@@ -9,28 +9,41 @@ import kotlin.concurrent.fixedRateTimer
 
 class MainActivity : AppCompatActivity() {
     private var timer: Timer? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val t = findViewById<TextView>(R.id.clock)
-        findViewById<Button>(R.id.left).setOnClickListener {
-            timer = fixedRateTimer("default", false, 0L, 1000){
-                val x = t.text.split(":")
-                var c = x[2].toInt()+1
-                var b = x[1].toInt()
-                if (c>59) {
-                    b += c/60
-                    c %= 60
-                }
-                var a = x[0].toInt()
-                if (b>59) {
-                    a += b/60
-                    b %= 60
-                }
-                runOnUiThread {
-                    t.text = applicationContext.getString(R.string.clock, a, b, c)
+    private var t: TextView? = null
+    private var l: Button? = null
+    private var r: Button? = null
+    private fun start() {
+        if(timer == null) {
+            timer = fixedRateTimer("default", false, 0L, 1000) {
+                t?.let {
+                    val x = it.text.split(":")
+                    var c = x[2].toInt() + 1
+                    var b = x[1].toInt()
+                    if (c > 59) {
+                        b += c / 60
+                        c %= 60
+                    }
+                    var a = x[0].toInt()
+                    if (b > 59) {
+                        a += b / 60
+                        b %= 60
+                    }
+                    runOnUiThread {
+                        it.text = applicationContext.getString(R.string.clock, a, b, c)
+                    }
                 }
             }
         }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        t = findViewById(R.id.clock)
+        l = findViewById(R.id.left)
+        r = findViewById(R.id.right)
+        l?.setOnClickListener{
+            start()
+        }
+        start()
     }
 }
