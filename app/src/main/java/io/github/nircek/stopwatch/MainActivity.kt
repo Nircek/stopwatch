cbe885ce13f2b824b2619a61d9d6e14ab5f09475
Nircek <nircek-2103@protonmail.com>
2019-03-25 15:51:52 +0100

    Fix saving Stopwacthes data

--- app/src/main/java/io/github/nircek/stopwatch/MainActivity.kt
+++ app/src/main/java/io/github/nircek/stopwatch/MainActivity.kt
@@ -4,29 +4,30 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.AppCompatImageButton
+import android.widget.ImageButton
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-    private var addNewStopwatch: AppCompatImageButton? = null
+    private var addNewStopwatch: ImageButton? = null
     private var sws: Stopwatches? = null
     private var sp: SharedPreferences? = null
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(null)
         setContentView(R.layout.activity_main)
         sws = Stopwatches(supportFragmentManager, R.id.stopwatches)
         sp = getSharedPreferences(packageName, Context.MODE_PRIVATE)
         addNewStopwatch = findViewById(R.id.add_new_stopwatch)
-        sws?.clear()
-
         try {
             sp?.getString("Stopwatches", "false,0")?.let { sws?.fromString(it) }
-        } catch(e: Throwable) { Log.e(this.toString(), e.toString()); sws?.new() }
+        } catch (e: Throwable) {
+            Log.e(this.toString(), e.toString());
+            sws?.new()
+        }
         addNewStopwatch?.setOnClickListener { sws?.new() }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onPause() {
+        super.onPause()
         sp?.edit()?.putString("Stopwatches", sws?.toString())?.apply()
     }
 }
--- app/src/main/java/io/github/nircek/stopwatch/Stopwatches.kt
+++ app/src/main/java/io/github/nircek/stopwatch/Stopwatches.kt
@@ -4,7 +4,7 @@ import android.support.v4.app.FragmentManager
 
 class Stopwatches (private val ft: FragmentManager, private val wh: Int) {
     private val list: MutableList<Stopwatch> = mutableListOf()
-    fun add(x: Stopwatch) {
+    private fun add(x: Stopwatch) {
         x.parent = this
         list.add(x)
         ft.beginTransaction().add(wh, x, x.fragID).commit()
@@ -14,10 +14,6 @@ class Stopwatches (private val ft: FragmentManager, private val wh: Int) {
         list.removeAll { it.fragID == x.fragID }
         x.parent = null
         x.destroy()
-    }
-    fun clear() {
-        for (e in list)
-            destroy(e)
     }
     override fun toString() = list.joinToString(";")
     fun fromString(l: String) {
